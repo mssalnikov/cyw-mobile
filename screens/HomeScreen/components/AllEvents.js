1e54e107c5199f7b005a33cba0b5a9b34c83567d
zuldare <mishasalnikov@gmail.com>
2018-10-01 00:30:57 +0300

    save

---
+++ screens/HomeScreen/components/AllEvents.js
@@ -0,0 +1,65 @@
+import React from 'React'
+import styled from 'styled-components/native'
+import { connect }  from 'react-redux'
+
+import Container from 'components/Container'
+import Events from 'components/Events'
+import Button from 'components/Button'
+import { createGetMyEvents } from '../../../state/actions/events';
+
+class AllEvents extends React.Component {
+    state = {
+        pending: true
+    }
+
+    componentDidMount() {
+        this.props.getAllEvents()
+    }
+
+    static getDerivedStateFromProps(props) {
+        if (props.events.success) {
+            return { pending: false }
+        } else {
+            return { pending: true }
+        }
+    }
+
+    render() {
+        const { pending } = this.state
+        if (pending) return <Container></Container>
+
+        const { events: { data: events }} = this.props 
+        return (
+            <Container>
+                {events.length == 0 && 
+                    <Text>
+                        You don't have events yet
+                    </Text>
+                }
+                {
+                    events.length > 0 &&
+                    <Events 
+                        events={events}
+                    />
+                }
+                <Button title={'Create'} onPress={() => this.props.navigation.navigate("CreateEvent")}/>
+            </Container>
+        )
+    }
+}
+
+const Text = styled.Text`
+    font-size: 16px;
+`
+
+const mapStateToProps = state => ({
+    events: state.my_events
+})
+
+const mapDispatchToProps = dispatch => ({
+    getAllEvents: () => dispatch(createGetMyEvents())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllEvents)
+
+
--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,24 +1,12 @@
 import React    from 'react'
 import { createBottomTabNavigator } from 'react-navigation'
-import { Image }    from 'react-native'
-import Events   from 'components/Events'
 import styled from 'styled-components'
 
 import MyEvents from './components/MyEvents'
+import AllEvents from './components/AllEvents'
 
 const My = MyEvents
-
-const Near = () => <Events events={[
-    { title: 'Easy girl looking for a man, can you find me?'},
-    { title: '5g pineapple express in two hops'},
-    { title: 'Easter scavenger hunt 18+'},
-    { 
-        title: 'Speak easy bar only for physicists',
-        image: 'http://1.bp.blogspot.com/-EIqqVOCuIcE/T5HfIXEk05I/AAAAAAAAARk/NzQrgFJ5itI/s1600/einstein.jpg',
-        description: 'Reimagining of Schrödinger\'s Cat Breaks Quantum Mechanics—and Stumps Physicists. Pulsar Discoverer Jocelyn Bell Burnell Wins $3-Million Breakthrough Prize. The award recognizes not only the astrophysicist’s transformative discovery, but also her subsequent work to promote equality and diversity in science.'
-    },
-    { title: 'Quest in reality starts here!'},
-]} />
+const All = AllEvents
 
 
 export default createBottomTabNavigator({
@@ -29,7 +17,7 @@ export default createBottomTabNavigator({
         }
     },
     All: {
-        screen: Near,
+        screen: All,
         navigationOptions: {
             tabBarIcon: () => <Icon source={require('../../assets/images/tabs/near.png')} />
         }
